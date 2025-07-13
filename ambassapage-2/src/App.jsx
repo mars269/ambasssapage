55fd0a045cfa53da69cf92a2ef5b56523ef8c458
mars269 <marsm1007@gmail.com>
2025-07-12 21:02:40 -0700

    Add files via upload

---
+++ ambassapage-2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Ambassapage!</h1>
+      <p>This is the starter React app. We'll build your ambassador project here.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ ambassapage-2/src/pages/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to Ambassapage 🌟</h1>
+      <p>This is your ambassador guide website!</p>
+    </div>
+  );
+}
